e0b8ee38f70606dfb12d4d8e88081820e2b94d33
Vikas Sarvasya <85037039+SarvasyaVikas@users.noreply.github.com>
2023-08-23 23:51:53 -0400

    Add files via upload

--- AnnotateVideo.py
+++ AnnotateVideo.py
@@ -8,17 +8,11 @@ import time
 
 vid = cv2.VideoCapture('InForm-5VID-Data-MP4.mp4')
 
-LXs = []
-RXs = []
-TYs = []
-BYs = []
+centersX = []
+centersY = []
 
-LX = 0
-TY = 0
-RX = 0
-BY = 0
-	
-counter = 0
+cX = 0
+cY = 0
 
 def Interact(action, x, y, flags, *userdata):
 	global counter
@@ -29,74 +23,49 @@ def Interact(action, x, y, flags, *userdata):
 
 	if action == cv2.EVENT_LBUTTONDBLCLK:
 		print('CLICK')
-		if counter >= 0:
-			counter += 1
-			if counter == 1:
-				LX = x
-				TY = y
-			elif counter == 2:
-				RX = x
-				BY = y
-				temp = image.copy()
-				cv2.rectangle(temp, (LX, TY), (RX, BY), (0, 0, 255), 2)
-				cv2.imshow("Image", temp)
-				cv2.waitKey(10)
-				
-	if action == cv2.EVENT_MBUTTONDOWN:
-		counter = 0
-		LX = 0
-		TY = 0
-		RX = 0
-		BY = 0
-		temp = image.copy()
-		print("RESET")
+		cX = x
+		cY = y
+		print([cX, cY])
+		cv2.circle(frame, (cX, cY), radius = 5, color = (0, 0, 255), thickness = -1)
 	
-	if counter == 1:
-		temp = image.copy()
-		cv2.rectangle(temp, (LX, TY), (x, y), (0, 255, 0), 2)
-		init = time.time()
-		while (time.time() - init) < 0.01:
-			cv2.imshow("Image", temp)
+	if action == cv2.EVENT_MBUTTONDOWN:
+		cX = 0
+		cY = 0
+		print("RESET")
 
 
 cv2.namedWindow("Image")
 cv2.setMouseCallback("Image", Interact)
 
+fr = 0
 while (vid.isOpened()):
     ret, frame = vid.read()
     if ret:
-        while True:
-            cv2.imshow("Frame", frame)
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                    counter = 0
-                    break
-
-        lx = min(LX, RX)
-        rx = max(LX, RX)
-        ty = min(TY, BY)
-        by = max(TY, BY)
-
-        LXs.append(lx)
-        RXs.append(rx)
-        TYs.append(ty)
-        BYs.append(by)
-
-        else:
-            break
+    	if fr % 5 == 0:
+    		while True:
+    			frame = cv2.resize(frame, (640, 360))
+    			cv2.imshow("Image", frame)
+    			if cv2.waitKey(10) & 0xFF == ord('q'):
+    				print("NEXT")
+    				print(fr)
+    				break
+    			
+    			centersX.append(cX)
+    			centersY.append(cY)
+    fr += 1
 
 vid.release()
 
 csvfile = open("InForm-Tennis-Serve-AC.csv", "a+")
 csvwriter = csv.writer(csvfile)
 
-for row in range(len(LXs)):
+for row in range(len(centersX)):
+	print("start save")
 	dtype = "Tennis_Serve"
 	lst = [dtype, row + 1]
 	for i in range(4):
-		lst.append(LXs[row + 1])
-		lst.append(TYs[row + 1])
-		lst.append(RXs[row + 1])
-		lst.append(BYs[row + 1])
+		lst.append(centersX[row + 1])
+		lst.append(centersY[row + 1])
 	
 	csvwriter.writerow(lst)
 
